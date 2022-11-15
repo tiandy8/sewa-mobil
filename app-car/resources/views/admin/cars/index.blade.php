5578902074befd55cf4e5aa7aeda82e8e723967c
tiandy8 <christiandynathannael.rpla@gmail.com>
2022-11-15 23:47:44 +0700

    menit 1:28:34

---
+++ app-car/resources/views/admin/cars/index.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="card">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h3>Daftar Mobil</h3>
+            <a href="{{ route('cars.create') }}" class="btn btn-primary">Tambah Data</a>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" >
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Mobil</th>
+                            <th>Gambar Mobil</th>
+                            <th>Harga Mobil</th>
+                            <th>Status Mobil</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($cars as $car)
+
+                            <tr>
+                                <td>{{ $loop->iteration }}</td>
+                                <td>{{ $car->nama_mobil }}</td>
+                                <td>
+                                    <img src="{{ Storage::url($car->gambar) }}" width="200"  alt="">
+                                </td>
+                                <td>{{ $car->harga_sewa }}</td>
+                                <td>{{ $car->status }}</td>
+                                <td>
+                                    <a href="{{ route('cars.edit', $car->id) }}" class="btn btn-sm btn-warning">Edit</a>
+                                </td>
+                            </tr>
+                            @empty
+                                <tr>
+                                    <td class="text-center" colspan="6">Data Kosong</td>
+                                </tr>
+                            @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+@endsection
